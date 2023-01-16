c77f41cdbc6338337ebd6d66661b0d367247adb4
jamesvrooney <jamesvrooney@hotmail.com>
2023-01-16 20:53:42 +0000

    Added basic security config.

--- src/main/resources/db/changelog/sql/add-test-users.sql
+++ src/main/resources/db/changelog/sql/add-test-users.sql
@@ -1,3 +1,3 @@
 insert into users (id, username, password) values
-(1, 'james', 'password'),
-(2, 'john', 'password');
+(1, 'james', '$2a$12$DoqMke2G3PnDT1ml6QJite9Jbqu2ElX9BM7JnAv69qvLOGIP5XzWm'),
+(2, 'john', '$2a$12$yorN.YXjlolPk5XYaWkfP.izeW/SD7BYbogcwgsfBeAZAfXkeQrEW');
