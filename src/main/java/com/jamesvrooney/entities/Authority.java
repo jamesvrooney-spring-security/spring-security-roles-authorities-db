96dada42a23c32937daa58e533aeb4ac74941325
jamesvrooney <jamesvrooney@hotmail.com>
2023-01-16 21:03:10 +0000

    Added entities.

---
+++ src/main/java/com/jamesvrooney/entities/Authority.java
@@ -0,0 +1,28 @@
+package com.jamesvrooney.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "authorities")
+public class Authority {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<User> users;
+}
---
+++ src/main/java/com/jamesvrooney/entities/User.java
@@ -0,0 +1,32 @@
+package com.jamesvrooney.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_authorities",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
+    private Set<Authority> authorities;
+}
