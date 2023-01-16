5f9c22cdca926b58f6d639110323fc87b69edb94
jamesvrooney <jamesvrooney@hotmail.com>
2023-01-16 21:09:39 +0000

    Added repositories.

---
+++ src/main/java/com/jamesvrooney/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.jamesvrooney.repositories;
+
+import com.jamesvrooney.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("""
+        SELECT u FROM USER u WHERE u.username = :username
+    """)
+    Optional<User> findByUsername(String username);
+}
